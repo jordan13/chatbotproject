0499e730e56d0634b73aad478e304d30c0e70093
Jordan <jajaber13@gmail.com>
2016-12-22 15:10:44 -0800

    responses activated
    done with the responses

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,20 @@
-function echoFunction() {
-  document.getElementById("chat-area").textContent = document.getElementById("input").value;
+var nice = document.getElementById("input").value;
+var time = new Date();
+console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+// var test = "how old are you";
+
+function echoFunction(nice, now) {
+  // var test = "how old are you";
+  // document.getElementById("chat-area").textContent = document.getElementById("input").value;
+if(input.value == "how old are you") {
+  console.log(2);
+  document.getElementById("chat-area").textContent =
+       "17 years old";
+}else if (input.value == "what is your name") {
+  document.getElementById("chat-area").textContent = "Jordan";
+}else if(input.value == "what time is it") {
+  document.getElementById("chat-area").textContent = "Here is the time and date" + " " + time;
+}
+
+
 }
